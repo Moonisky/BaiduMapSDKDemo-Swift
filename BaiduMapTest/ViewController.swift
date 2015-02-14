38c045c410c717b0c76cc4847b604f66e2a73e6b
星夜暮晨 <semper_idem@126.com>
2015-02-14 23:36:45 +0800

    Initial Commit

---
+++ BaiduMapTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaiduMapTest
+//
+//  Created by Semper Idem on 15-2-14.
+//  Copyright (c) 2015年 益行人-星夜暮晨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
