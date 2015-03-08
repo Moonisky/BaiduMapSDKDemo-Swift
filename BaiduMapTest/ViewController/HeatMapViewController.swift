3acfa6a7681ee2fe380d3f1a6532addd99d91ce0
星夜暮晨 <semper_idem@126.com>
2015-03-09 00:00:53 +0800

    1、完成了全部的 View Controller 设计和 View 设计 2、实现了 geo 编码和反编码功能

---
+++ BaiduMapTest/ViewController/HeatMapViewController.swift
@@ -0,0 +1,57 @@
+//
+//  HeatMapViewController.swift
+//  BaiduMapTest
+//
+//  Created by Semper Idem on 15-3-8.
+//  Copyright (c) 2015年 益行人-星夜暮晨. All rights reserved.
+//
+
+import UIKit
+
+class HeatMapViewController: UIViewController, BMKMapViewDelegate {
+    
+    /// 百度地图视图
+    var mapView: BMKMapView!
+    
+    // 界面加载
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 设置标题
+        self.title = arrayOfDemoName[13]
+        
+        // 地图界面初始化
+        mapView = BMKMapView(frame: view.frame)
+        mapView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.view.addSubview(mapView)
+        
+        // 界面初始化
+        
+        // 创建地图视图约束
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(NSLayoutConstraint(item: mapView, attribute: .Leading, relatedBy: .Equal, toItem: view, attribute: .Leading, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: mapView, attribute: .Trailing, relatedBy: .Equal, toItem: view, attribute: .Trailing, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: mapView, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1, constant: 0))
+        //constraints.append(NSLayoutConstraint(item: mapView, attribute: .Top, relatedBy: .Equal, toItem: btn_Search, attribute: .Bottom, multiplier: 1, constant: 8))
+        self.view.addConstraints(constraints)
+    }
+    
+    // MARK: - 协议代理设置
+    
+    override func viewWillAppear(animated: Bool) {
+        mapView.viewWillAppear()
+        mapView.delegate = self  // 此处记得不用的时候需要置nil，否则影响内存的释放
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        mapView.viewWillDisappear()
+        mapView.delegate = nil  // 不用时，置nil
+    }
+    
+    // MARK: - 内存管理
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
